6c0ca8e2d6f13b08d11d5c34c5442edff50ac426
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-29 15:00:35 +0100

    Create test to view player 2 hit points, implement base player 2 hit points functionality

--- spec/features/infrastructure_spec.rb
+++ spec/features/infrastructure_spec.rb
@@ -1,14 +1,16 @@
-describe "homepage view", :type => :feature do
-  # it "displays that infrastructure is working" do
-  #   visit '/'
-  #   expect(page).to have_content 'Testing infrastructure working!'
-  # end
-
-  it 'enter player names' do
+feature "homepage view" do
+  before do
     visit '/'
     fill_in 'player_1', :with => 'Jessica'
     fill_in 'player_2', :with => 'Steph'
     click_button 'Battle!'
+  end
+
+  scenario 'view player names' do
     expect(page).to have_content 'Preparing Jessica and Steph to battle to the death'
+  end
+
+  scenario "displays player 2's hit points" do
+    expect(page).to have_content "Steph's hp: 100"
   end
 end
