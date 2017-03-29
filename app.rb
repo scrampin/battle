fec3e0ea0f6a274cf5e955b10a2316c0e51471a6
Stephanie Crampin <steph.crampin@gmail.com>
2017-03-29 15:59:42 +0100

    refactored attack button

--- app.rb
+++ app.rb
@@ -21,7 +21,8 @@ class Battle < Sinatra::Base
     erb :play
   end
 
-  post '/attack' do
+  get '/attack' do
+    @player_1 = session['player_1']
     @player_2 = session['player_2']
     erb :attack
   end
--- spec/features/attack_player_2_spec.rb
+++ spec/features/attack_player_2_spec.rb
@@ -2,8 +2,8 @@ feature "attack player 2" do
 
   scenario "player 1 attacks player 2" do
     sign_in_and_play
-    click_button 'Slap!'
-    expect(page).to have_content "You slapped Steph!"
+    click_link 'Slap!'
+    expect(page).to have_content "Jessica slapped Steph!"
   end
 
 end
