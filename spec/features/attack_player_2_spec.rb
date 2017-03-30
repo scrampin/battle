bb2af2fc25f528c08178306fc3c079b0307cbda9
Stephanie Crampin <steph.crampin@gmail.com>
2017-03-30 15:14:50 +0100

    refactored to include current player indicator tests

--- spec/features/attack_player_2_spec.rb
+++
@@ -1,9 +0,0 @@
-feature "attack player 2" do
-
-  scenario "player 1 attacks player 2" do
-    sign_in_and_play
-    click_link 'Slap!'
-    expect(page).to have_content "Ruan slapped Steph!"
-  end
-
-end
---
+++ spec/features/attack_player_spec.rb
@@ -0,0 +1,21 @@
+feature "attack player" do
+
+  scenario "player 1 attacks player 2" do
+    sign_in_and_play
+    click_link 'Slap!'
+    expect(page).to have_content "Ruan slapped Steph!"
+  end
+
+  scenario "current player name should display on play page" do
+    sign_in_and_play
+    expect(page).to have_content "Ruan's turn to attack!"
+  end
+
+  scenario "current player name should display on play page" do
+    sign_in_and_play
+    click_link 'Slap!'
+    click_link 'Back to play'
+    expect(page).to have_content "Steph's turn to attack!"
+  end
+
+end
